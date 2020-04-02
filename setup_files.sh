d3679166092bd35d3f4a7c0bd88219eb370e6ab8
badranx <yahya.badran@gmail.com>
2020-04-02 16:08:39 +0200

    Fixes after testing on Linux:Lubuntu

--- setup_files.sh
+++ setup_files.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
 LOGS="./files_mistakes.log"
 MyName="badranx"
-NVIM="$HOME/.config/nvim/"
+NVIM="$HOME/.config/nvim"
 RedShift="$HOME/.config/redshift"
 APPEND_TO_BASHRC="bashrc_append"
 
 #print error message to LOGS
 iserror() {
 	if [ $? -ne 0 ]; then
-		echo "$1" |  tee -a LOGS
+		echo "$1" |  tee -a $LOGS
+	else
+		echo "successfully copied $1"
 	fi
 }
 
-#check if .bashrc contain "badranx"
-if grep -Fxq "$MyName" "$HOME/.bashrc"
-then
-	cat "./file/APPEND_TO_BASHRC" | tee -a "$HOME/.bashrc"
-fi
+#a code that append changes to the variable PS1 that affects the terminal prompt
+##check if .bashrc contain "badranx"
+#if [ ! $(grep -Fxq "$MyName" "$HOME/.bashrc") ]
+#then
+#	cat "./file/$APPEND_TO_BASHRC" | tee -a "$HOME/.bashrc"
+#fi
 
 cp -f file/.tmux.conf "$HOME"
 iserror ".tmux.conf"
-mkdir -p $NVIM && cp -f  "./file/.init.vim" $NVIM
+mkdir -p $NVIM && cp -f  "./file/init.vim" $NVIM
 iserror "$NVIM/.init.vim"
-mkdir -p $RedShift && cp -f "./file/.redshift.conf" $RedShift
+mkdir -p $RedShift && cp -f "./file/redshift.conf" $RedShift
 iserror "$RedShift/.redshift.conf"
--- setup_packages.sh
+++ setup_packages.sh
@@ -1,34 +1,49 @@
 #!/bin/bash
-
+#assume you already downloaded nodejs
+PACKAGE_MANAGER_CMD="apt-get -yf install"
+PIP3_CMD="pip3 install -Ur"
 LOGS="./packages_mistakes.log"
-
+PIP_REQUIREMENTS="./pip_requirements.txt"
+#for MatplotLib, After this Matplotlib will install the required FreeType on its own
+MPLLOCALFREETYPE=1
 #print error message to LOGS
 iserror() {
 	if [ $? -ne 0 ]; then
-		echo "$1" |  tee -a LOGS
+		echo "$1" >&2 |  tee -a $LOGS
 	fi
 }
 
 apt-get update
 
-rm -f LOGS
+rm -f $LOGS
 
 while IFS= read -r line; do
-	apt-get -y install  $line
+	[[ $line = \#* ]] && continue
+	[[ $line = *"build-dep" ]] && PACKAGE_MANAGER_CMD="apt-get build-dep -yf install"
+	$PACKAGE_MANAGER_CMD  $line
 	iserror "$line"
 done < "$1"
 
 #Python requirments
 #Jupyterlab requirements
-pip3 install -r ./requirements.txt
+$PIP3_CMD  $PIP_REQUIREMENTS
 if [ $? -ne 0 ]; then
 	echo "badranx error: mistake installing python requirements"
 else
+	
+	#the following plugin needs npm
 	jupyter labextension install jupyterlab_vim
 
 	iserror "jubyterlab_vim (plugin)"
 fi
 
-#install nodjes as sugested by coc-vim
-curl -sL install-node.now.sh/lts | bash
-iserror "install nodejs"
+#install neovim
+add-apt-repository ppa:neovim-ppa/unstable
+iserror("BadranX: problem with PPA:neovim")
+apt-get update
+apt-get install neovim
+iserror("BadranX: problem with installing neovim")
+#neovim Plugin manager (Vim-Plug)
+curl "-fLo" "$HOME/.local/share/nvim/site/autoload/plug.vim" --create-dirs "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
+iserror("BadranX: prolem with installing Vim-Plug for neovim")
+
